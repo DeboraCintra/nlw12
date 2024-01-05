51f179541be5727377af654502c364697616f581
DeboraCintra <dbrcl.30@gmail.com>
2024-01-05 10:42:35 -0300

    aula 4.2 ajuste botao cadastrar memorias

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -4,8 +4,14 @@ const singInURL = `https://github.com/login/oauth/authorize?client_id=${process.
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
+
+
   if (!token) {
-    return NextResponse.redirect(singInURL)
+    return NextResponse.redirect(singInURL, {
+      headers:{
+        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
+      },
+    })
   }
   return NextResponse.next()
 }
