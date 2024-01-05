87e1249c2fea6f26b75ac8ee4a08793da218c648
DeboraCintra <dbrcl.30@gmail.com>
2024-01-05 11:25:12 -0300

    aula 4.3 criacao layouts mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,19 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { useEffect } from 'react';
-import { ImageBackground, View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
-import { styled } from 'nativewind';
 import * as SecureStore from 'expo-secure-store';
 import { useRouter } from 'expo-router'
-
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
 import { api } from '../src/lib/api';
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-const StyleStripes = styled(Stripes)
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { useFonts } from 'expo-font';
+
 // Endpoint
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -23,7 +19,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-  const [hasLoadedFonts] = useFonts({Roboto_400Regular,Roboto_700Bold,BaiJamjuree_700Bold,})
   
   const [,response, singInWithGitHub] = useAuthRequest(
     {
@@ -35,17 +30,17 @@ export default function App() {
     },
     discovery
   );
+
   async function handleGithubOAuthCode(code: string) {
     const response = await api.post('/register', {
       code,
     })
-
     const { token } = response.data
-
     await SecureStore.setItemAsync('token', token)
 
     router.push('/memories')
   }
+  
   useEffect(() => {
     if (response?.type === 'success') {
       const { code } = response.params
@@ -54,16 +49,9 @@ export default function App() {
     }
   }, [response]);
 
-  if (!hasLoadedFonts) {
-    return null
-  }
   return (
-    <ImageBackground
-      source={blurBg}
-      className='relative  flex-1 items-center bg-gray-900 px-8 py-10'
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyleStripes className='absolute left-2' />
+    <View className='flex-1 items-center px-8 py-10'>
+    
       <View className='flex-1 items-center justify-center gap-6'>
 
         <NLWLogo />
@@ -90,8 +78,8 @@ export default function App() {
         Feito com 💜 no NLW da Rocketseat
       </Text>
 
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+      
+    </View>
   );
 }
 
