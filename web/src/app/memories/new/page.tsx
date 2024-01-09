8acb7f8ca04b16ad22ec20eed388d37986cf5b4c
DeboraCintra <dbrcl.30@gmail.com>
2024-01-09 10:42:04 -0300

    aula 5.0 preview image anexo

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,3 +1,4 @@
+import { MediaPicker } from "@/components/MediaPicker";
 import { Camera, ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
@@ -17,15 +18,20 @@ export default function NewMemory() {
             Anexar media
           </label>
           <label htmlFor="isPublic" className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100">
-            <input type="checkbox" name="isPublic" id="isPublic" value="true" className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"/>
+            <input
+              type="checkbox"
+              name="isPublic"
+              id="isPublic" 
+              value="true" 
+              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500" />
             Tornar memória pública
           </label>
         </div>
-        <input type="file" id="media" className="invisible w-0 h-0"></input>
-        <textarea name="content" spellCheck={false} 
-                  className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100
+        <MediaPicker />
+        <textarea name="content" spellCheck={false}
+          className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100
                    placeholder:text-gray-400 focus:ring-0"
-                  placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."/>
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre." />
 
       </form>
     </div>
