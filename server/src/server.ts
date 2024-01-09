4561c69dead4f8de4c5a2a4761399a8047e05034
DeboraCintra <dbrcl.30@gmail.com>
2024-01-08 21:15:19 -0300

    aula 4.6 upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,23 +6,25 @@ import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
 app.register(multipart)
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 app.register(cors, {
   origin: true,
 })
-
 app.register(jwt, {
   secret: 'spacetime',
 })
-
 app.register(authRoutes)
 app.register(memoriesRoutes)
 app.register(uploadRoutes)
-
 app
   .listen({
     port: 3333,
